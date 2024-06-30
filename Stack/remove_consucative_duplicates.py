4fed570dfc3a75c91b145a8162c8e7d931f6b27c
Vikash Kumar <vikash1998bscc@gmail.com>
2024-06-30 21:57:48 +0530

    Create stack with list and remove consucative duplicate pairs solution

---
+++ Stack/remove_consucative_duplicates.py
@@ -0,0 +1,30 @@
+"""
+Given a string. Remove every consucative dublicate pair of chars until there are no consucative duplicate pair.
+s = "acddck"
+out -> "ak"
+
+s = "abckkcbadmmc"
+out -> "dc"
+"""
+from stack import StackWithList
+
+
+def remove_consucative_duplicates(string):
+    stack = StackWithList()
+    for chr in string:
+        if stack.peak() == chr:
+            stack.pop()
+        else:
+            stack.push(chr)
+    return stack.to_string()
+
+def main():
+    string = "acddck"
+    result = remove_consucative_duplicates(string)
+    print(result, "==================")
+
+    string = "abckkcbadmmc"
+    result = remove_consucative_duplicates(string)
+    print(result, "==================")
+
+main()
---
+++ Stack/stack.py
@@ -0,0 +1,22 @@
+from typing import Any
+
+class StackWithList:
+    def __init__(self):
+        self.stack = []
+        self.top = -1
+
+    def push(self, item) -> None:
+        self.stack.append(item)
+        self.top += 1
+
+    def pop(self) -> None:
+        if self.top == -1:
+            return
+        self.stack.pop()
+        self.top -= 1
+    
+    def peak(self) -> Any:
+        return None if self.top == -1 else self.stack[self.top]
+
+    def to_string(self):
+        return "".join(self.stack)
