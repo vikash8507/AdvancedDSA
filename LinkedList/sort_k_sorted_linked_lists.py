441072d3175fdb6fa5664e4e4e9a803c55d0c7e4
Vikash Kumar <vikas.kumar@mitratech.com>
2024-07-02 17:15:47 +0530

    Added some more questions related to linked list

---
+++ LinkedList/sort_k_sorted_linked_lists.py
@@ -0,0 +1,11 @@
+"""
+Sort a k sorted doubly linked list
+
+Given a doubly linked list containing n nodes, where each node is at most k away from its target position in the list. The problem is to sort the given doubly linked list. 
+For example, let us consider k is 2, a node at position 7 in the sorted doubly linked list, can be at positions 5, 6, 7, 8, 9 in the given doubly linked list.
+
+Original Doubly linked list:
+3 6 2 12 56 8 
+Doubly linked list after sorting:
+2 3 6 8 12 56
+"""
